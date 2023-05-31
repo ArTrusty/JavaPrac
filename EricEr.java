4a3918d269496f0b7a1df3a36ffa30d5f8f1231b
ArTrusty <110997809+ArTrusty@users.noreply.github.com>
2023-05-31 12:10:27 -0400

    Add files via upload

---
+++ EricEr.java
@@ -0,0 +1,13 @@
+
+public class EricEr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	int[] a = { 1,2,3};
+	int[] b = {1,2,3};
+	System.out.println(a==b);
+	}
+
+}
